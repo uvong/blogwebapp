cd07e10de960cb32f7016d91e5aeea66df995142
Uyen Vong <uyenvong@Uyens-MBP.users.gowaveg.com>
2021-08-08 19:32:43 -0700

    added custom error pages

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,9 +2,8 @@ import os
 from decouple import config
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY')#'1104570453007afbe0960d9f562a001f'
-    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')#'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.gmail.com'
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
